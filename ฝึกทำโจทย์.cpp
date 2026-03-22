5dcd0195f1b1c69defbe407ef2f025f3600b9af0
folk <chanathip.raksat@gmail.com>
2026-03-22 21:45:03 +0700

    ทำเทสข้อใหม่

--- ฝึกทำโจทย์.cpp
+++ ฝึกทำโจทย์.cpp
@@ -1,14 +1,36 @@
 #include <iostream>
 using namespace std;
-int solve(int n) {
-    if (n == 1) return 1;
-    if (n == 2) return 2;
-    return solve(n - 1) + solve(n - 2); 
-}
+
+int arr[105][105];
+int dp[105][105];
+
 int main() {
-    int m;
-    cin >> m;
-    int method = solve(m);
-    cout << method;
+    int n, m;
+    cin >> n >> m;
+    
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    dp[0][0] = arr[0][0];
+
+    for (int j = 0; j < m; j++) {
+        dp[0][j] = dp[0][j-1] + arr[0][j];
+    }
+
+    for (int i = 0; i < n; i++) {
+        dp[i][0] = dp[i - 1][0] + arr[i][0];
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            dp[i][j] = arr[i][j] + max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+    cout << dp[n - 1][m - 1];
     return 0;
+    
 }
