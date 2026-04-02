eb29b253b39f50be0f2465e9a0a14845aa2fcd46
Tawan <s32481@utw.ac.th>
2026-04-02 20:25:32 +0700

    add initial implementation of name input program

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std ;
+int main()
+{
+    string name ;
+    
+    cout << "Enter name : " ;
+    cin >> name ;
+    cout << "Name : " << name ;
+    return 0 ;
+}
