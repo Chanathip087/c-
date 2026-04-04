0dd08fa44e4bc281f6981c2c30e9cee89b68ca0b
folk <chanathip.raksat@gmail.com>
2026-04-04 20:16:53 +0700

    newcomeback

--- comeback.cpp
+++ comeback.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+int sumarray(vector <int> a) {
+    int sum = 0;
+    for (int x : a) {
+        sum += x;
+    }
+    return sum;
+}
 int main() {
     int n;
+    vector <int> a;
     cin >> n;
-    for (int i = 0; i <= n; i++) {
-        if (i % 2 != 0) {
-            cout << i << " ";
-        }
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        a.push_back(x);
     }
+    cout << sumarray(a) << endl;
     return 0;
 }
