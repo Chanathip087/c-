a5dc62858d1b2634a6e89f08f01a445b2da75be8
folk <chanathip.raksat@gmail.com>
2026-04-12 22:42:58 +0700

    newgreedy

--- practice/greedy.cpp
+++ practice/greedy.cpp
@@ -2,23 +2,45 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+
+struct Job {
+    int deadline;
+    int score;
+};
+
+bool cmp(Job a, Job b) {
+    return a.score > b.score; // เรียงคะแนนจากมากไปน้อย
+}
+
 int main() {
-    int f;
-    int k;
-    cin >> f >> k;
-    vector <int> a;
-    for (int i = 0; i < f; i++) {
-        int x;
-        cin >> x;
-        a.push_back(x);
+    int n;
+    cin >> n;
+
+    vector<Job> jobs(n);
+    int maxDeadline = 0;
+
+    for (int i = 0; i < n; i++) {
+        cin >> jobs[i].deadline >> jobs[i].score;
+        maxDeadline = max(maxDeadline, jobs[i].deadline);
     }
 
-    sort(a.begin(), a.end(), greater<int>());
-    int sum = 0;
-    
-    for (int i = 0; i < k; i++) {
-        sum += a[i];
+    sort(jobs.begin(), jobs.end(), cmp);
+
+    vector<bool> used(maxDeadline + 1, false);
+    int totalScore = 0;
+
+    for (int i = 0; i < n; i++) {
+        
+        for (int t = jobs[i].deadline; t >= 1; t--) {
+            if (!used[t]) {
+                used[t] = true;
+                totalScore += jobs[i].score;
+                break;
+            }
+        }
     }
-    cout << sum << endl;
+
+    cout << totalScore << endl;
+
     return 0;
 }
